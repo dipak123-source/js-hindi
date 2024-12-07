e74a82ad9fe8b2a5a2fe57407298d183636ca741
dipak123-source <dipakpaswan9635@gmail.com>
2024-12-07 05:18:46 +0000

    datatypes

---
+++ first_Project/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict" // treat all js code as newer version
+//alert(3+3)// we are using node js here that's why we don't use alert, if we use browser here that it is applicable
+let name = "Dipak";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+let uge = undefined;
+console.log(typeof uge);
